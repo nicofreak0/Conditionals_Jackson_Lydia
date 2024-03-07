e644d348c58bf3c5d574a06d12beada9c1665a03
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-03-07 15:08:24 -0600

    s

--- Program.cs
+++ Program.cs
@@ -6,8 +6,26 @@ namespace Conditionals_Jackson_Lydia
     {
         static void Main(string[] args)
         {
+
+            // asks user to enter whole number and assigns to int variable
             Console.WriteLine("Enter a whole number.");
             int myWholeNum = Convert.ToInt32(Console.ReadLine());
+
+            // checks if positive or negative
+            if (myWholeNum >= 0) 
+            {
+                Console.WriteLine($"{myWholeNum} is a positive number.");
+            }
+            else
+            {
+                Console.WriteLine($"{myWholeNum} is a negative number.");
+            }
+
+            // checks if even or odd
+            if (myWholeNum % 2 == 0) 
+            {
+                Console.WriteLine($"{myWholeNum} is an odd number.");
+            }
 
 
         }
